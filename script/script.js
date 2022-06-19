f1b8ebe68262dbdb4f6518a665793313c4b1e7cc
imhalid <imhalid@icloud.com>
2022-06-20 00:57:49 +0300

    some changes

--- script/script.js
+++ script/script.js
@@ -1,4 +1,5 @@
 const selectedNumber = document.querySelectorAll(".number");
+const submitButton = document.querySelector("#submit");
 
 const hangleSelect = (el) => {
   selectedNumber.forEach((item) => {
@@ -7,9 +8,16 @@ const hangleSelect = (el) => {
   el.classList.add("selected");
 };
 
+let selectedNowNumber = 0;
+
 selectedNumber.forEach((item) => {
   item.addEventListener("click", (e) => {
     hangleSelect(e.target);
-    console.log(e.target.innerHTML);
+    selectedNowNumber = e.target.innerHTML;
+    // console.log(selectedNowNumber);
   });
+});
+
+submitButton.addEventListener("click", () => {
+  console.log(selectedNowNumber);
 });
